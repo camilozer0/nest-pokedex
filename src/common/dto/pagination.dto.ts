0ab7f5aeac4385f4c2a3de86a3599afacc32f10d
Camilo Mazorra <mazorra006@gmail.com>
2023-09-22 17:35:40 -0500

    query params en get

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,16 @@
+import { IsOptional, Min, IsPositive, IsNumber } from "class-validator";
+
+
+export class PaginationDto {
+    
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    @Min(1)
+    limit?: number;
+
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    offset?: number;
+}
